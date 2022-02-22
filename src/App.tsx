dbd93917b058e56731b93367e2154bf794f93970
yingyangcode <marcel.anis.21@gmail.com>
2022-02-22 00:16:14 -0500

    Add initial styling and canvas

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import React, { useRef, useEffect } from "react";
+import { clearCanvas, setCanvasSize } from "./utils/canvasUtils";
+
+const WIDTH = 1024;
+const HEIGHT = 768;
+
+function App() {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const getCanvasWithContext = (canvas = canvasRef.current) => {
+    return { canvas, context: canvas?.getContext("2d") };
+  };
+  useEffect(() => {
+    const { canvas, context } = getCanvasWithContext();
+    if (!canvas || !context) {
+      return;
+    }
+    setCanvasSize(canvas, WIDTH, HEIGHT);
+    context.lineJoin = "round";
+    context.lineCap = "round";
+    context.lineWidth = 5;
+    context.strokeStyle = "black";
+
+    clearCanvas(canvas);
+  }, []);
+
+  return (
+    <div className="window">
+      <div className="title-bar">
+        <div className="title-bar-text">Redux Paint</div>
+        <div className="title-bar-controls">
+          <button aria-label="Close" />
+        </div>
+      </div>
+      <canvas ref={canvasRef} />
+    </div>
+  );
+}
+
+export default App;
