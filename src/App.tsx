1cb526ec0464659b8a155836846eada05b3dd551
yingyangcode <marcel.anis.21@gmail.com>
2022-02-27 21:51:44 -0500

    using slices

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { drawStroke, clearCanvas, setCanvasSize } from "./utils/canvasUtils";
-import { beginStroke, updateStroke } from "./modules/currentStroke/actions";
+import { beginStroke, updateStroke } from "./modules/currentStroke/slice";
 import { endStroke } from "./modules/sharedActions";
-import { strokesSelector } from "./modules/strokes/reducer";
-import { currentStrokeSelector } from "./modules/currentStroke/reducer";
-import { historyIndexSelector } from "./modules/historyIndex/reducer";
+import { strokesSelector } from "./modules/strokes/slice";
+import { currentStrokeSelector } from "./modules/currentStroke/slice";
+import { historyIndexSelector } from "./modules/historyIndex/slice";
 import { EditPanel } from "./shared/EditPanel";
 import { ColorPanel } from "./shared/ColorPanel";
 import { FilePanel } from "./shared/FilePanel";
